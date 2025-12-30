3c163b97dbee5cef5140557d4128fad6a6d3e408
rafaalmeida1 <rafael.almeida@backlgrs.com.br>
2025-12-30 01:31:21 -0300

    refactor: remove unused import from CreatePixKeyModal for cleaner code

--- src/components/debt/CreatePixKeyModal.tsx
+++ src/components/debt/CreatePixKeyModal.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { pixKeysService, CreatePixKeyDto } from '@/services/pixKeys.service';
